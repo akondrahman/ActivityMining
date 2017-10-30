76dbd435f48e008cae0d8d6fef92a73584ec2d0d
Akond Rahman <akond.rahman.buet@gmail.com>
2017-10-30 18:12:20 -0400

    mining build count  data

--- src/cluster_session_buildmining.py
+++ src/cluster_session_buildmining.py
@@ -7,7 +7,30 @@ Build data mining for clustered sessions
 import os
 import cPickle as pickle
 import pandas as pd
-import utils 
+import utils
+
+def dumpValuesToFile(list_param, file2save):
+    str2write = ''
+    for elem in list_param:
+        str2write = str2write + str(elem) + ',' + '\n'
+    feature_name = file2save.split('.')[0]
+    str2write = feature_name + ',' + '\n' + str2write
+    os_bytes = utils.dumpContentIntoFile(str2write, file2save)
+    print 'DUMPED A FILE OF {} BYTES'.format(os_bytes)
+    return os_bytes
+
+def getBuildCountForClusters(sess_with_labels_dict):
+    build_ds_path = '/Users/akond/Documents/AkondOneDrive/MSR18-MiningChallenge/output/datasets/LOCKED_ALL_BUILD_CONTENT.csv'
+    build_df   = pd.read_csv(build_ds_path)
+    high_grp, low_grp = [], []
+    for sess_id, sess_label in sess_with_labels_dict.iteritems():
+        matched_build_df = build_df[build_df['SESS_ID']==sess_id]
+        build_event_cnt = len(matched_build_df.index)
+        if sess_label==1:
+           high_grp.append(build_event_cnt)
+        else:
+           low_grp.append(build_event_cnt)
+    return high_grp, low_grp
 
 if __name__=='__main__':
     print "Started at:", utils.giveTimeStamp()
@@ -19,10 +42,10 @@ if __name__=='__main__':
             high_count += 1
     print 'Total:{}, High:{}, Low:{}'.format(len(final_sess_with_labels), high_count, len(final_sess_with_labels) - high_count)
     print '='*50
-    # h_grp_edit_cnt, l_grp_edit_cnt = getEditCountForClusters(final_sess_with_labels)
-    # dumpValuesToFile(h_grp_edit_cnt, 'H_EDIT_COUNT.csv')
-    # dumpValuesToFile(l_grp_edit_cnt, 'L_EDIT_COUNT.csv')
-    # print 'Edit count data extracted ...'
-    # print '='*50
-    # utils.compareTwoGroups(h_grp_edit_cnt, l_grp_edit_cnt, 'EDIT_COUNT')
-    # print '='*50
+    h_grp_build_cnt, l_grp_build_cnt = getBuildCountForClusters(final_sess_with_labels)
+    dumpValuesToFile(h_grp_build_cnt, 'H_BUILD_COUNT.csv')
+    dumpValuesToFile(l_grp_build_cnt, 'L_BUILD_COUNT.csv')
+    print 'Build count data extracted ...'
+    print '='*50
+    utils.compareTwoGroups(h_grp_build_cnt, l_grp_build_cnt, 'BUILD_COUNT')
+    print '='*50
