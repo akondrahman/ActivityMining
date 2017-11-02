232a64bdb5d42f958d77702fa1343b3a26e358fb
Akond Rahman <akond.rahman.buet@gmail.com>
2017-11-02 16:52:45 -0400

    debug code for sessions

---
+++ src/cluster_session_debug.py
@@ -0,0 +1,33 @@
+'''
+Akond Rahman
+Nov 02, 2017
+Thursday
+Debugging for sessions
+'''
+
+import os
+import pandas as pd
+import utils
+import datetime,  time
+import cPickle as pickle
+import numpy as np
+from collections import Counter
+
+def makeTimeHuman(single_val):
+    #2016-05-17T01:28:02.8130763+02:00 , timestamp string
+    dt_  = single_val.split('T')[1]
+    ts_  = dt_.split('+')[0]
+    x = time.strptime(ts_.split('.')[0],'%H:%M:%S')
+    second2ret = datetime.timedelta(hours=x.tm_hour, minutes=x.tm_min, seconds=x.tm_sec).total_seconds()
+    return second2ret
+
+
+def dumpValuesToFile(list_param, file2save):
+    str2write = ''
+    for elem in list_param:
+        str2write = str2write + str(elem) + ',' + '\n'
+    feature_name = file2save.split('.')[0]
+    str2write = feature_name + ',' + '\n' + str2write
+    os_bytes = utils.dumpContentIntoFile(str2write, file2save)
+    print 'DUMPED A FILE OF {} BYTES'.format(os_bytes)
+    return os_bytes
