5af2f3ecd1b54261a131d0eb0fe571a7cfb6d5c7
Akond Rahman <akond.rahman.buet@gmail.com>
2017-11-02 17:38:29 -0400

    debug code for sessions

--- src/cluster_session_debug.py
+++ src/cluster_session_debug.py
@@ -77,6 +77,49 @@ def getDebugIntervalForClusters(sess_with_labels_dict):
 
     return high_grp, low_grp
 
+def getNormalizedDebugReason(list_):
+    dict2Ret, finalDict = {}, {}
+    temp_debug_reason_cnt_holder = []
+    for sess_data in list_:
+        dist_, reason_cnt = sess_data
+        temp_debug_reason_cnt_holder.append(reason_cnt)
+        for d_reason, reason_cnt in dist_.iteritems():
+            if d_reason not in dict2Ret:
+               dict2Ret[d_reason] = [reason_cnt]
+            else:
+               dict2Ret[d_reason] = dict2Ret[d_reason] + [reason_cnt]
+    #print dict2Ret
+    all_reason_cnt = sum(temp_debug_reason_cnt_holder)
+    #print all_reason_cnt
+    for type_, tot_cnt in dict2Ret.iteritems():
+        if type_ not in finalDict:
+            percentage       = round(float(sum(tot_cnt))/float(all_reason_cnt), 5)*100
+            finalDict[type_] = percentage
+    return finalDict
+
+
+def getDebugReasonsForClusters(sess_with_labels_dict):
+    debug_ds_path = '/Users/akond/Documents/AkondOneDrive/MSR18-MiningChallenge/output/datasets/LOCKED_ALL_DEBUG_CONTENT.csv'
+    debug_df      = pd.read_csv(debug_ds_path)
+    high_grp, low_grp = [], []
+    for sess_id, sess_label in sess_with_labels_dict.iteritems():
+        matched_debug_df = debug_df[debug_df['SESS_ID']==sess_id]
+        # need to filter out debug actions that occur for exceptions
+        non_exception_debug_df = matched_debug_df[matched_debug_df['ACTION']=='dbgExecutionActionDefault']
+        debug_event_cnt = len(non_exception_debug_df.index)
+
+        debug_types = non_exception_debug_df['REASON'].tolist()
+        debug_type_dist = dict(Counter(debug_types))
+        debug_type_tup  = (debug_type_dist, debug_event_cnt)
+        if sess_label==1:
+           high_grp.append(debug_type_tup)
+        else:
+           low_grp.append(debug_type_tup)
+    h_d_dist_dict = getNormalizedDebugReason(high_grp)
+    l_d_dist_dict = getNormalizedDebugReason(low_grp)
+    return h_d_dist_dict, l_d_dist_dict
+
+
 if __name__=='__main__':
     print "Started at:", utils.giveTimeStamp()
     print '='*100
@@ -104,3 +147,13 @@ if __name__=='__main__':
     print '='*50
     utils.compareTwoGroups(h_grp_debug_int, l_grp_debug_int, 'NORM_DEBUG_INTERVAL')
     print '='*50
+    h_dist_dict, l_dist_dict = getDebugReasonsForClusters(final_sess_with_labels)
+    print 'HIGH (NUMBERS ARE IN %):'
+    print h_dist_dict
+    print '-'*50
+    print 'LOW (NUMBERS ARE IN %):'
+    print l_dist_dict
+    print '-'*50
+    print '='*100
+    print "Ended at:", utils.giveTimeStamp()
+    print '='*100
