3d6698e97faed393307b6712b2b7824039a84204
Akond Rahman <akond.rahman.buet@gmail.com>
2017-12-07 13:03:49 -0500

    added summation

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,7 @@ import cliffsDelta
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
-import os 
+import os
 
 def makeBoxPlots(h_list, l_list, feature_param):
     data_to_plot = [h_list, l_list,]
@@ -22,9 +22,9 @@ def makeBoxPlots(h_list, l_list, feature_param):
     plt.close()
 
 def compareTwoGroups(h_list, l_list, feature_name):
-   print 'HIGH ({}): (count:{}, median:{}, mean:{})'.format(feature_name, len(h_list), np.median(h_list), np.mean(h_list))
+   print 'HIGH ({}): (count:{}, median:{}, mean:{}, sum:{})'.format(feature_name, len(h_list), np.median(h_list), np.mean(h_list), sum(h_list))
    print '-'*25
-   print 'LOW ({}): (count:{}, median:{}, mean:{})'.format(feature_name, len(l_list), np.median(l_list), np.mean(l_list))
+   print 'LOW ({}): (count:{}, median:{}, mean:{}, sum:{})'.format(feature_name, len(l_list), np.median(l_list), np.mean(l_list), sum(l_list))
    print '-'*25
    greater_TS, greater_p = stats.mannwhitneyu(h_list, l_list, alternative='greater')
    cliffs_delta = cliffsDelta.cliffsDelta(h_list, l_list)
