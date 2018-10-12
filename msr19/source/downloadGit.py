2caa93fa4f62d56f72de56ee91ef26ae01b9ace8
Akond Rahman <akond.rahman.buet@gmail.com>
2018-10-12 10:26:53 -0400

    python downloader

--- msr19/source/downloadGit.py
+++ msr19/source/downloadGit.py
@@ -3,7 +3,7 @@ Oct 12, 2018
 Akond Rahman 
 Script to download Python files 
 '''
-
+import os 
 import urllib
 import pandas as pd 
 import numpy as np 
@@ -11,13 +11,20 @@ import numpy as np
 def downloadFiles(df_, out_dir_):
     testfile = urllib.URLopener()
     python_file_links =  np.unique( df_['pythonLink'].tolist() )
-    cnt = 1
+    cnt, err_cnt = 1, 1
     for link_ in python_file_links:
          file2save = link_.split('/')[-1]
          dir2save  = link_.split('/')[-2]
-         out_fil   = out_dir_ + dir2save + '/' + file2save 
-         testfile.retrieve(link_, out_fil)
-         print 'Download completed for:{}, {} processed so far ...'.format(file2save, cnt)
+         dir_      = out_dir_ + dir2save 
+         if not os.path.exists(dir_):
+            os.makedirs(dir_)
+         out_fil   = dir_ + '/' + file2save 
+         try:
+            testfile.retrieve(link_, out_fil)
+            print 'Download completed for:{}, {} processed so far ...'.format(file2save, cnt)
+         except IOError: 
+            print 'So far not downloaded :{} python files'.format(err_cnt)
+            err_cnt += 1 
          cnt += 1 
 
 if __name__=='__main__':
