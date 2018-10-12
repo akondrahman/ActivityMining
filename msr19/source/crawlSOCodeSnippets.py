4aac44154bed184dbf30e839b76970ab04732ad2
Akond Rahman <akond.rahman.buet@gmail.com>
2018-10-12 13:43:42 -0400

    SO downloader

--- msr19/source/crawlSOCodeSnippets.py
+++ msr19/source/crawlSOCodeSnippets.py
@@ -25,7 +25,7 @@ def getCode(html_data):
     return code_snippets
 
 def getTitle(html_data):
-    unparsed_dump = html_data.body.find('title')
+    unparsed_dump = html_data.title
     code_ = str(unparsed_dump)
     if (('>' in code_) and ('<' in code_)):
             title_ = code_.split('>')[1].split('<')[0]
@@ -33,10 +33,19 @@ def getTitle(html_data):
             title_ = 'NOT_FOUND'            
     return title_    
 
-def getPostDetails(df_, out_dir):
+def getPostDetails(df_, out_dir, py_out_dir_):
+    out_dir = {}
     python_file_links =  np.unique( df_['pythonLink'].tolist() )
     for link_ in python_file_links:
          so_post_IDs = df_[df_['pythonLink']==link_]['sopostID'].tolist()
+
+         file2save = link_.split('/')[-1]
+         dir2save  = link_.split('/')[-2]
+         dir_      = py_out_dir_ + dir2save 
+         if not os.path.exists(dir_):
+            os.makedirs(dir_)
+         out_fil   = dir_ + '/' + file2save 
+         post_details = []
          for postID in so_post_IDs:
             full_url  = 'https://stackoverflow.com/questions/' + str(postID) + '/'
             response_ = urllib2.urlopen(full_url)
@@ -44,15 +53,21 @@ def getPostDetails(df_, out_dir):
             parsed_html    = BeautifulSoup(html_dump)
             code_snippets  = getCode(parsed_html)
             title          = getTitle(parsed_html)
-            print full_url, title
-            print code_snippets
-            print '='*50
-
+            # print out_fil, full_url, title
+            # print code_snippets
+            # print '='*50
+            post_details.append((full_url, title, code_snippets))
+         if out_fil not in out_dir:
+           out_dir[out_fil] = post_details
+    return out_dir
 
 
 
 if __name__=='__main__':
-   the_dat = '/Users/akond/Documents/AkondOneDrive/MSR-MiningChallenge/msr19/data/OUT_FIL_GH_DAT.csv'
-   the_df  = pd.read_csv(the_dat)
-   out_dir = '/Users/akond/Documents/AkondOneDrive/MSR-MiningChallenge/msr19/data/so_code_snippets/'
-   getPostDetails(the_df, out_dir) 
+   the_dat     = '/Users/akond/Documents/AkondOneDrive/MSR-MiningChallenge/msr19/data/OUT_FIL_GH_DAT.csv'
+   the_df      = pd.read_csv(the_dat)
+   out_dir     = '/Users/akond/Documents/AkondOneDrive/MSR-MiningChallenge/msr19/data/so_code_snippets/'
+   py_out_dir  = '/Users/akond/Documents/AkondOneDrive/MSR-MiningChallenge/msr19/data/python_files/'
+   
+   post_gh_dic = getPostDetails(the_df, out_dir, py_out_dir) 
+   pickle.dump( post_gh_dic, open( out_dir + 'GH.PY.SO.TRACKER.PKL', 'wb'))          
